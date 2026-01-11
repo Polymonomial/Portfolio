44fddb83444e347b57e7e5e9eab4c6664432a2ad
Brendan Chang <brendanjchang@yahoo.com.hk>
2026-01-10 23:01:02 -0500

    changed image path from using pathes to imports so it works on vercel

--- src/components/headerfooter/footer.tsx
+++ src/components/headerfooter/footer.tsx
@@ -1,11 +1,14 @@
+import linkedinIcon from '/src/assets/InBug-White.png';
+import emailIcon from '/src/assets/email.webp';
+import githubIcon from '/src/assets/github-mark-white.png';
 function Footer() {
   return (
     <footer className="bg-gray-800 text-white py-4 overflow-auto flex-1">
       <div className="container mx-auto text-center">
         <div className="flex justify-center items-center gap-4 mb-2">
-          <a href="https://www.linkedin.com/in/brendan-chang-44b1a71a2" target="_blank"><img src="/src/assets/InBug-White.png" alt="linkedin Icon" className="w-6 h-6 hover:scale-125 transition-transform duration-200"/></a>
-          <a href="mailto:brendanjchang@yahoo.com.hk"><img src="/src/assets/email.webp" alt="Email Icon" className="w-6 h-6 hover:scale-125 transition-transform duration-200"/></a>
-          <a href="https://github.com/Polymonomial" target="_blank"><img src="/src/assets/github-mark-white.png" alt="GitHub Logo" className="w-6 h-6 hover:scale-125 transition-transform duration-200"/></a>
+          <a href="https://www.linkedin.com/in/brendan-chang-44b1a71a2" target="_blank"><img src={linkedinIcon} alt="linkedin Icon" className="w-6 h-6 hover:scale-125 transition-transform duration-200"/></a>
+          <a href="mailto:brendanjchang@yahoo.com.hk"><img src={emailIcon} alt="Email Icon" className="w-6 h-6 hover:scale-125 transition-transform duration-200"/></a>
+          <a href="https://github.com/Polymonomial" target="_blank"><img src={githubIcon} alt="GitHub Logo" className="w-6 h-6 hover:scale-125 transition-transform duration-200"/></a>
         </div>
         <p className="font-sans font-thin text-xs">BRENDAN CHANG &copy; 2026</p>
       </div>
--- src/components/projects/projects.tsx
+++ src/components/projects/projects.tsx
@@ -1,3 +1,4 @@
+import fragmentIcon from '/src/assets/fragment_ss.png';
 function About() {
     return (
         <div>
@@ -5,7 +6,7 @@ function About() {
              <div className="w-full max-w-6xl mx-auto p-8 flex">
                 
                 <div className="flex-col flex flex-1 items-center pr-20 justify-center">
-                    <a href="https://github.com/Polymonomial/fragments" target="_blank"><img className="hover:scale-105 hover:shadow-xl hover:brightness-110 transition-all duration-300" src="src/assets/fragment_ss.png" alt="AWS Microservices Mockup" /></a>
+                    <a href="https://github.com/Polymonomial/fragments" target="_blank"><img className="hover:scale-105 hover:shadow-xl hover:brightness-110 transition-all duration-300" src={fragmentIcon} alt="AWS Microservices Mockup" /></a>
                 </div>
                 <div className="flex flex-col mr-8 flex-1 items-end">
                     <div className="h-24 relative"><h1 className="text-4xl font-bold mb-4 relative z-10">AWS microservices mockup</h1><div className="absolute top-0 left-0 font-sans bg-gradient-to-br from-red-500 to-yellow-500 h-5 w-18 z-0"></div>   </div>
